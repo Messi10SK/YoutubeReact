feadf2819622a13d3270c9e7236d73e493fac88d
Satyam Kanojiya <kanojiyasatyam07@gmail.com>
2024-02-07 16:08:58 +0530

    file Added couinter project

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+ 
+const [count,Setcount] = useState(15)
+
+const addValue =()=>{
+  Setcount(count+1 < 20 ? count+1 : 20)
+}
+
+
+const removeValue =()=>{
+  Setcount(count-1 > 0 ? count-1 : 0)
+}
+
+  return (
+    <>
+
+  <h1>Satyam Kanojiya</h1>
+
+  <h2>Counter Value:{count}</h2>
+
+  <button onClick={addValue}>Add Value</button>
+
+  <br />
+
+  <button onClick={removeValue}>remove Value</button>
+
+    </>
+
+  )
+}
+
+export default App;
